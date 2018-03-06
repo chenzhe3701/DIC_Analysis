ad01f81437d5234cc6c31b98ba405338db6bd560
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2018-03-06 02:33:01 -0800

    modify 2_auxF, so it can plot rooted_sum_squared, so it can use both cluster_centroid-vs-twin_strain, and cluster_medium_strain-vs-twin_strain. But comment on new code, just use it as an option.

--- trace_analysis_2_auxF_dissimilarity_vs_sf.m
+++ trace_analysis_2_auxF_dissimilarity_vs_sf.m
@@ -28,22 +28,31 @@ iE_stop = 5;
 
 for iE = iE_start:iE_stop
     
-    name_result_on_the_fly = [sampleName,'_s',num2str(STOP{iE+B}),'_cluster_result_on_the_fly.mat'];
+    name_result_on_the_fly = [sampleName,'_s',num2str(STOP{iE+B}),'_cluster_result_on_the_fly.mat'];    % with or without 'fly_rss'
     load([saveDataPath,name_result_on_the_fly]);
     
-    dissimilarity = [];
+    rtss_cCen_tStrain = [];
+    rtss_cMed_tStrain = [];
     schmidfactor = [];
     for iS=1:length(stru)
         nCluster = length(stru(iS).cLabel);
         for iCluster = 1:nCluster
-            dissimilarity  = [dissimilarity, pdist2(stru(iS).cCen(iCluster,:), stru(iS).tStrain)];
+            rtss_cCen_tStrain  = [rtss_cCen_tStrain, pdist2(stru(iS).cCen(iCluster,:), stru(iS).tStrain)];
+            % rtss_cMed_tStrain  = [rtss_cMed_tStrain, pdist2(stru(iS).cMed(iCluster,:), stru(iS).tStrain)];
             schmidfactor = [schmidfactor, stru(iS).tSF];
             
         end
     end
-    figure;plot(dissimilarity,schmidfactor,'.');
+    figure;plot(rtss_cCen_tStrain,schmidfactor,'.');
     title(['Strain level: ',num2str(STOP{iE+B})]);
-    xlabel('Dissimilarity');
+    xlabel('RootSS Cluster Center - Twin Strain');
     ylabel('Schmid Factor');
+    
+%     figure;plot(rtss_cMed_tStrain,schmidfactor,'.');
+%     title(['Strain level: ',num2str(STOP{iE+B})]);
+%     xlabel('RootSS Cluster Medium - Twin Strain');
+%     ylabel('Schmid Factor');
+%     
+%     figure; histogram(rtss_cCen_tStrain - rtss_cMed_tStrain);
     
 end
