e012a25d00f5e6357797a3dc442f495752cb0bdb
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2018-04-07 04:04:15 -0700

    add note, slight modify, and change name, for the script to show all training images for the CNN.

--- script_2nd_paper_2_show_cnn_training_images.m
+++ script_2nd_paper_2_show_cnn_training_images.m
@@ -1,6 +1,12 @@
+% chenzhe, 2018-04-07, add note
+% script to make mosiac images showing the training images for the CNN.
+
 p = uigetdir('D:\WE43_T6_C1_insitu_compression\Analysis_by_Matlab\train_img_color\twin','training image Dir for twins');
 p2 = uigetdir('D:\WE43_T6_C1_insitu_compression\Analysis_by_Matlab\train_img_color\notwin','training image Dir for twins');
 
+savePath = uigetdir('','select a dir to save the plots');
+
+%%
 list = dir(p);
 ii = 1;
 filenames = [];
@@ -13,7 +19,7 @@ while ii<= length(list)
    end
 end
 figure; montage(filenames,'size',[10,5]); %title('Traning Images for Twins');
-
+print(fullfile(savePath,'train image nontwins.tif'),'-dtiff');
 
 list = dir(p2);
 ii = 1;
@@ -27,3 +33,4 @@ while ii<= length(list)
    end
 end
 figure; montage(filenames,'size',[10,10]); %title('Traning Images for Non-Twins');
+print(fullfile(savePath,'train image twins.tif'),'-dtiff');
