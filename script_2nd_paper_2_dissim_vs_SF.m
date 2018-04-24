1fb637bff9f9411266ed0a800d8cc6137fa580a7
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2018-04-24 08:47:50 -0700

    modify so that in post analysis, it can (1) plot only the best matched cases to look at SF vs Dissimilarity. (2) use different colors for them

--- script_2nd_paper_2_dissim_vs_SF.m
+++ script_2nd_paper_2_dissim_vs_SF.m
@@ -10,6 +10,11 @@ addChenFunction;
 [fileSetting,pathSetting] = uigetfile('','select setting file which contains sampleName, stopNames, FOVs, translations, etc');
 load_settings([pathSetting,fileSetting],'sampleName','cpEBSD','cpSEM','sampleMaterial','stressTensor');
 
+saveFig = 1;
+if saveFig
+    saveFigurePath = [uigetdir('C:\Users\ZheChen\Desktop','choose a path to save the figures'),'\'];
+end
+
 % load previous data and settings
 saveDataPath = [uigetdir('','choose a path [to save the]/[of the saved] processed data, or WS, or etc.'),'\'];
 load([saveDataPath,sampleName,'_traceAnalysis_WS_settings.mat']);
@@ -25,36 +30,57 @@ iE_start = 2;   % elongation levels to analyze. 0-based.
 iE_stop = 5;
 
 %% plot dissimilarity vs schmid factor 
+postAnalysis = 0;
 SF = [];
 Dis = [];
+Twinned = [];
 for iE = iE_start:iE_stop
     
     name_result_on_the_fly = [sampleName,'_s',num2str(STOP{iE+B}),'_cluster_result_on_the_fly.mat'];    % with or without 'fly_rss'
+    if postAnalysis
+        name_result_on_the_fly = [sampleName,'_s',num2str(STOP{iE+B}),'_cluster_to_twin_result.mat'];       % use this after labeled
+    end
     load([saveDataPath,name_result_on_the_fly]);
     
     rtss_cCen_tStrain = [];
     rtss_cMed_tStrain = [];
     schmidfactor = [];
+    trueTwin = [];
     for iS=1:length(stru)
         nCluster = length(stru(iS).cLabel);
         for iCluster = 1:nCluster
-            rtss_cCen_tStrain  = [rtss_cCen_tStrain, pdist2(stru(iS).cCen(iCluster,:), stru(iS).tStrain)];
-            % rtss_cMed_tStrain  = [rtss_cMed_tStrain, pdist2(stru(iS).cMed(iCluster,:), stru(iS).tStrain)];
-            schmidfactor = [schmidfactor, stru(iS).tSF];
-            
+            if postAnalysis
+                % if it is later identified as a twin
+                if stru(iS).trueTwin(iCluster)>0
+                    trueTwin = [trueTwin,1];
+                else
+                    trueTwin = [trueTwin,2];
+                end
+                rtss_cCen_tStrain  = [rtss_cCen_tStrain, stru(iS).dis(iCluster)];
+                schmidfactor = [schmidfactor, stru(iS).sf(iCluster)];
+            else
+                allAvailable = 0;   
+                if allAvailable
+                    rtss_cCen_tStrain  = [rtss_cCen_tStrain, pdist2(stru(iS).cCen(iCluster,:), stru(iS).tStrain)];
+                    % rtss_cMed_tStrain  = [rtss_cMed_tStrain, pdist2(stru(iS).cMed(iCluster,:), stru(iS).tStrain)];
+                    schmidfactor = [schmidfactor, stru(iS).tSF];
+                else
+                    d = pdist2(stru(iS).cCen(iCluster,:), stru(iS).tStrain);
+                    [d,ind] = min(d);
+                    rtss_cCen_tStrain  = [rtss_cCen_tStrain,d];
+                    % rtss_cMed_tStrain  = [rtss_cMed_tStrain, pdist2(stru(iS).cMed(iCluster,:), stru(iS).tStrain)];
+                    schmidfactor = [schmidfactor, stru(iS).tSF(ind)];
+                end
+            end
         end
     end
     Dis{iE} = rtss_cCen_tStrain;
     SF{iE} = schmidfactor;
+    Twinned{iE} = trueTwin;
 end
+
 
 %% plot
-saveFig = 1;
-if saveFig
-    saveDataPath = [uigetdir('D:\WE43_T6_C1_insitu_compression\Analysis_by_Matlab','choose a path [to save the]/[of the saved] processed data, or WS, or etc.'),'\'];
-end
-
-%%
 close all;
 titleString = {'','Global Uniaxial Strain = -0.6%','Global Uniaxial Strain = -1.2%',...
     'Global Uniaxial Strain = -2.1%', 'Global Uniaxial Strain = -3.7%'};
@@ -66,7 +92,15 @@ titleString = {'',...
 
 for iE = iE_start:iE_stop
     
-    figure;plot(Dis{iE},SF{iE},'.');
+    if postAnalysis
+       figure; hold on;
+       plot(Dis{iE}(Twinned{iE}==2),SF{iE}(Twinned{iE}==2),'.b','markersize',8);
+       plot(Dis{iE}(Twinned{iE}==1),SF{iE}(Twinned{iE}==1),'.r','markersize',8);
+       legend({'Non-twinned','Twinned'},'Position',[0.3 0.25 0.3 0.14]);
+    else
+        figure;plot(Dis{iE},SF{iE},'.','markersize',8);
+    end
+    
     title(titleString{iE},'fontweight','normal','fontsize',18);
     xlabel('Dissimilarity');
     xlabel('\psi^D_{min}');
@@ -77,17 +111,23 @@ for iE = iE_start:iE_stop
     
     imgName = (['s',num2str(iE),'_Dis_vs_SF.tif']);
     if saveFig
-        print(fullfile(saveDataPath,imgName),'-dtiff');   % to parent folder
+        print(fullfile(saveFigurePath,imgName),'-dtiff');   % to parent folder
         
         imgNameA = (['s',num2str(iE),'_Dis_vs_SF_a.tif']);
         title(['Strain Level: ',num2str(STOP{iE+B})],'fontweight','normal','fontsize',18);
-        print(fullfile(saveDataPath,imgNameA),'-dtiff');   % to parent folder
+        print(fullfile(saveFigurePath,imgNameA),'-dtiff');   % to parent folder
         close all;
     end
     
     % zoom-in view
     asp = daspect;
-    figure;plot(Dis{iE},SF{iE},'.');
+    if postAnalysis
+       figure; hold on;
+       plot(Dis{iE}(Twinned{iE}==2),SF{iE}(Twinned{iE}==2),'.b','markersize',10);
+       plot(Dis{iE}(Twinned{iE}==1),SF{iE}(Twinned{iE}==1),'.r','markersize',10);
+    else
+        figure;plot(Dis{iE},SF{iE},'.','markersize',10);
+    end
     hold on; fplot(@(x) 7*x+0.15,[0 0.05],'--k','linewidth',5);
     set(gca,'fontsize',18);
     set(gca,'xlim',[0 0.08],'ylim',[0.2, 0.5]);
@@ -96,7 +136,7 @@ for iE = iE_start:iE_stop
     
     imgName = (['s',num2str(iE),'_Dis_vs_SF_zoom.tif']);
     if saveFig
-        print(fullfile(saveDataPath,imgName),'-dtiff');   % to parent folder
+        print(fullfile(saveFigurePath,imgName),'-dtiff');   % to parent folder
         close all;
     end
 %     figure;plot(rtss_cMed_tStrain,schmidfactor,'.');
