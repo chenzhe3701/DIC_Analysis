e1333f671006ba385623e05b6c47fe26bd0dc8f9
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2018-05-14 23:15:58 -0700

    temp codes. Add 'model_grain_boundary' to find keypoints to model grain boundaries.

--- model_grain_boundary.m
+++ model_grain_boundary.m
@@ -1,57 +1,18 @@
+% function [gb_dir, gb_s_pt, pt_pos, pt_s_gb, tripleLookup] = model_grain_boundary(ID,x,y,stepSize)
+% gb_dir{i} = 'horizontal' or 'vertical'
+% gb_s_pt{i} = [id of points belonging to this grain boundary]  
+% pt_pos(i,:) = [x,y of this point]
+% pt_s_gb{i} = [id of grain boundaries belongs to this point]
+%
 % chenzhe, 2018-05-14
-% In branch 'Ti7Al_B6'
-% Organize this to look at grain boundary alignment
 
-clear;
-addChenFunction;
-dicPath = uigetdir('E:\Ti7Al_B6_insitu_tension\stitched_DIC','pick DIC directory, which contains the stitched DIC data for each stop');
-dicFiles = dir([dicPath,'\*.mat']);
-dicFiles = struct2cell(dicFiles);
-dicFiles = dicFiles(1,:)';
-
-% looks like have to include this part to read the sample name.
-[fileSetting,pathSetting] = uigetfile('D:\p\m\DIC_Analysis\setting_for_real_samples','select setting file which contains sampleName, stopNames, FOVs, translations, etc');
-load_settings([pathSetting,fileSetting],'sampleName','cpEBSD','cpSEM','sampleMaterial','stressTensor');
-
-% load previous data and settings
-saveDataPath = [uigetdir('E:\Ti7Al_B6_insitu_tension\Analysis_by_Matlab','choose a path [to save the]/[of the saved] processed data, or WS, or etc.'),'\'];
-load([saveDataPath,sampleName,'_traceAnalysis_WS_settings.mat']);
-load([saveDataPath,sampleName,'_EbsdToSemForTraceAnalysis'],'gID','gNeighbors','gNNeighbors','ID','ID_0','iThick','x','y','X','Y','exx','gExx');
-
-gIDwithTrace = gID(~isnan(gExx));
-
-% modify / or keep an eye on these settings for the specific sample to analyze  ------------------------------------------------------------------------------------
-
-STOP = {'0','1','2','3','4','5','6','7','8','9','10','11','12','13','14'};
-B=1;    % 0-based B=1.  1-based B=0.
-iE_start = 2;   % elongation levels to analyze. 0-based.
-iE_stop = 14;
-% resReduceRatio = 3;         % to save space, reduce map resolution
-% grow_boundary_TF = 0;       % whether to grow boundary to make it thicker
-
-% file name prefixes
-f1 = 'Ti7Al_B6_s';
-f2 = '_';
-
-neighbor_elim = 1;          % don't consider this ID as neighbor. For example, ID = 1 or 0 means bad region.
-% twinTF_text = 'twin';        % do you want to analyze twin? Use things like 'twin' or 'notwin'
-
-%% build grain boundary model
-stepSize = 1;
-[gb_dir, gb_s_pt, pt_pos, pt_s_gb, tripleLookup] = model_grain_boundary(ID_0,x,y,stepSize);
-
-%% draw grain boundary, and make handles
-
-
-
-%%
-stepSize = 2;
-
-[boundaryTF,~,neighborID,tripleTF,~] = find_one_boundary_from_ID_matrix(ID_0);
+function [gb_dir, gb_s_pt, pt_pos, pt_s_gb, tripleLookup] = model_grain_boundary(ID,x,y,stepSize)
+% stepSize = 2
+[boundaryTF,~,neighborID,tripleTF,~] = find_one_boundary_from_ID_matrix(ID);
 tripleLookup = [x(tripleTF>0),y(tripleTF>0)];
 
 ind = boundaryTF>0;
-gbPoints = [ID_0(ind), neighborID(ind), x(ind), y(ind)];
+gbPoints = [ID(ind), neighborID(ind), x(ind), y(ind)];
 
 % sort the two grain IDs corresponding to gb points
 t = gbPoints(:,[1,2]);
@@ -80,7 +41,7 @@ for igb = 1:length(uniquePair)
         gb_dir{igb} = 'vertical';
     end
     
-    % determine up to 5 keypoints of this grain boundary segment for fitting 
+    % determine up to 5 keypoints of this grain boundary segment for fitting
     inds = round(linspace(1,size(segPts,1), min(5, size(segPts,1))));
     keyPts = segPts(inds,:);
     
@@ -101,7 +62,7 @@ for igb = 1:length(uniquePair)
         catch
             loc = 0;
         end
-
+        
         if loc > 0
             ipt = loc;
             ptCount = ptCount - 1;  % to conteract the ++ at the end of loop
@@ -128,12 +89,9 @@ for igb = 1:length(uniquePair)
         
         % [at the end of loop] increment ipt
         ptCount = ptCount + 1;
-
+        
     end
-
+    
 end
 
-
-
-
-
+end
