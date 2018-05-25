a04befc6e1e3e61d71ce5ac0746a00e678c94fd9
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2018-05-25 09:09:57 -0700

    move code to my Matlab function folder

--- Untitled.m
+++
@@ -1,6 +0,0 @@
-pxl_list_pre = pxl_cell{76}
-ind_list_pre = ind_cell{76}
-skl_list_pre = skl_cell{76}
-anchor_label_pre = anchor_label_cell{76}
-pxl_list, ind_list, skl_list
-save('fordebug_interp_between_layers','pxl_list_pre','ind_list_pre','skl_list_pre','anchor_label_pre','pxl_list','ind_list','skl_list')
--- script_understand_thinning.m
+++
@@ -1,8 +0,0 @@
-A=A;
-B=(bwmorph(A,'thin',1));
-C=(bwmorph(A,'thin',2));
-D=(bwmorph(A,'thin',3));
-E=(bwmorph(A,'thin',4));
-F=(bwmorph(A,'thin',5));
-figure;imagesc(A+B+C+D+E+F);
-
