dc27bc2693cbca2537cbf4f18f311862228cabd3
ZheChen-PC\ZheChen <chenzhezju@gmail.com>
2019-10-17 22:25:35 -0700

    2 methods to calculate the edmat, which are summaries of strain distribution at distances to gbs.

--- temp_useful_2019_10.m
+++ temp_useful_2019_10.m
@@ -118,12 +118,12 @@ xlabel('twin SF');
 ylabel('basal SF');
 
 %% 
+placeHolder = [];
 save('temp_results\saved_data.mat','placeHolder','-v7.3')
-%%
-save('temp_results\saved_data.mat','edmat_2','-append','-v7.3')
+
 %%
 D = matfile('temp_results\saved_data.mat')
-%% note, check, use -v7.3 file with outlier_removed=1.
+%% Method-1. Loop each grain, each neighbor (unique gb), calculate distance map. Then summarize e distribution.
 
 % calculate edmat: strain distribution matrix
 % [iE, ID_current, gb, nanmean of eEff @ d=1:250 data point distance to that gb.  
@@ -160,66 +160,31 @@ for iE = 2:5
         indR_max = min(size(ID,1), find(sum(ind_local, 2), 1, 'last')+1);
         
         ID_local = ID(indR_min:indR_max, indC_min:indC_max);
-        X_local = X(indR_min:indR_max, indC_min:indC_max);
-        Y_local = Y(indR_min:indR_max, indC_min:indC_max);
-        uniqueBoundary_local = uniqueBoundary(indR_min:indR_max, indC_min:indC_max);
-        boundaryTF_local = boundaryTF(indR_min:indR_max, indC_min:indC_max);
-        
+
         eMap_local = eMap(indR_min:indR_max, indC_min:indC_max);  % This is for effective strain
         
-        activeTS = cellfun(@(x) ~isempty(x), struCell{iE}(iS).tGb);
-        tSF = struCell{iE}(iS).tSF;
-        
-        % If grain of interest is twinned (here it means: got a twin-gb intersection labeled)
-        if any(cellfun(@(x) ~isempty(x), struCell{iE}(iS).tGb)) % sum(struCell{iE}(iS).cTrueTwin(:))>0
-            
-            % [[[[For each neighbor]  get stats about neighbor and plot, such as m'
-            for iNb = 1:nNeighbors
-                ID_neighbor = ID_neighbors(iNb);
-                iS_neighbor = find(arrayfun(@(x) x.gID == ID_neighbor, struCell{iE}));
-                if ~isempty(iS_neighbor)
-                    activeTS_Nb = cellfun(@(x) ~isempty(x), struCell{iE}(iS_neighbor).tGb);
-                    
-                    % (1.1) Calculate this_uniqueGB number.
-                    if ID_current > ID_neighbor
-                        gb = ID_current * 10000 + ID_neighbor;
-                    else
-                        gb = ID_neighbor * 10000 + ID_current;
-                    end
-                    
-                    % strain calculation in area of interest.
-                    distMap_local = distance_from_boundary_in_grain(ID_local, [gb,ID_current]);
-                    edmat = [edmat; iE, ID_current, gb, arrayfun(@(x) nanmean(eMap_local(distMap_local==x)), 1:250)];
-                    
-                end
-                % end of ~isempty(iS_neighbor)
+        % [[[[For each neighbor]  get stats about neighbor and plot, such as m'
+        for iNb = 1:nNeighbors
+            ID_neighbor = ID_neighbors(iNb);
+            iS_neighbor = find(arrayfun(@(x) x.gID == ID_neighbor, struCell{iE}));
+            if ~isempty(iS_neighbor)
                 
-            end
-            
-        else
-            % If grain of interest is not twinned, this means (iE_g > iE)
-            
-            for iNb = 1:nNeighbors
-                ID_neighbor = ID_neighbors(iNb);
-                
-                iS_neighbor = find(arrayfun(@(x) x.gID == ID_neighbor, struCell{iE}));
-                if ~isempty(iS_neighbor)
-                    % (1.1) Calculate this_uniqueGB number.
-                    if ID_current > ID_neighbor
-                        gb = ID_current * 10000 + ID_neighbor;
-                    else
-                        gb = ID_neighbor * 10000 + ID_current;
-                    end
-                    
-                    % strain calculation in area of interest.
-                    distMap_local = distance_from_boundary_in_grain(ID_local, [gb,ID_current]);
-                    edmat = [edmat; iE, ID_current, gb, arrayfun(@(x) nanmean(eMap_local(distMap_local==x)), 1:250)];
+                % (1.1) Calculate this_uniqueGB number.
+                if ID_current > ID_neighbor
+                    gb = ID_current * 10000 + ID_neighbor;
                 else
-                    disp(['neighbor not found: ',num2str(ID_neighbor)]);
+                    gb = ID_neighbor * 10000 + ID_current;
                 end
+                
+                % strain calculation in area of interest.
+                distMap_local = distance_from_boundary_in_grain(ID_local, [gb,ID_current]);
+                edmat = [edmat; iE, ID_current, gb, arrayfun(@(x) nanmean(eMap_local(distMap_local==x)), 1:250)];
+                
             end
+            % end of ~isempty(iS_neighbor)
+            
         end
-        
+
         % disp(['iE=',num2str(iE),', iS=',num2str(iS),', ID=',num2str(struCell{iE}(iS).gID)]);
         iS = iS + 1;
     end
@@ -242,16 +207,111 @@ for iE = 2:5
     end
     
 end
+%% Method-2. 
+% First calculate a distMap. Each data point is affected only by the nearest unique gb.
+% Then, Loop each grain, each neighbor (unique gb), crop the distance map. Then summarize e distribution.
 
-%%  Plot curve.
-iE = 2;
+% calculate edmat: strain distribution matrix
+% [iE, ID_current, gb, nanmean of eEff @ d=1:250 data point distance to that gb.  
+
+[~, boundaryID, neighborID, ~, ~] = find_one_boundary_from_ID_matrix(ID);
+uniqueBoundary = max(boundaryID,neighborID)*10000 + min(boundaryID,neighborID);
+
+[distMap, gbLabel] = city_block(uniqueBoundary);
+
+for iE = 2:5
+    
+    eMap = calculate_effective_strain(strainFile{iE-1}.exx, strainFile{iE-1}.exy, strainFile{iE-1}.eyy);
+    edmat = [];
+    
+    iS = 1;
+    warning('off','MATLAB:table:RowsAddedExistingVars');
+    continueTF = true;
+    dToTriple_th = 5;       % eliminate intersection whose distance to triple point is smaller than this value
+    dToTriple_th_to_label = dToTriple_th;  % label if distance of intersection to triple point is smaller than this value
+    
+    hW = waitbar(0, ['iE=',num2str(iE),' analyze each grain']);
+    hN = length(struCell{iE});
+    while (continueTF)&&(iS<=length(struCell{iE}))
+        waitbar(iS/hN, hW);
+        
+        close all;
+        ID_current = struCell{iE}(iS).gID
+        ind = find(gID==ID_current);
+        
+        nNeighbors = gNNeighbors(ind);
+        ID_neighbors = gNeighbors(ind, 1:nNeighbors);
+        
+        ind_local = ismember(ID, [ID_current, ID_neighbors]); %ismember(ID, [ID_current,ID_neighbor]);
+        
+        % Make it one data point wider on each side
+        indC_min = max(1, find(sum(ind_local, 1), 1, 'first')-1);
+        indC_max = min(size(ID,2), find(sum(ind_local, 1), 1, 'last')+1);
+        indR_min = max(1, find(sum(ind_local, 2), 1, 'first')-1);
+        indR_max = min(size(ID,1), find(sum(ind_local, 2), 1, 'last')+1);
+        
+        ID_local = ID(indR_min:indR_max, indC_min:indC_max);
+
+        eMap_local = eMap(indR_min:indR_max, indC_min:indC_max);  % This is for effective strain
+        gbLabel_local = gbLabel(indR_min:indR_max, indC_min:indC_max);
+        
+        % [[[[For each neighbor]  get stats about neighbor and plot, such as m'
+        for iNb = 1:nNeighbors
+            ID_neighbor = ID_neighbors(iNb);
+            iS_neighbor = find(arrayfun(@(x) x.gID == ID_neighbor, struCell{iE}));
+            if ~isempty(iS_neighbor)
+                
+                % (1.1) Calculate this_uniqueGB number.
+                if ID_current > ID_neighbor
+                    gb = ID_current * 10000 + ID_neighbor;
+                else
+                    gb = ID_neighbor * 10000 + ID_current;
+                end
+                
+                % strain calculation in area of interest.
+                distMap_local = distMap(indR_min:indR_max, indC_min:indC_max);
+                mask = (gbLabel_local==gb)&(ID_local==ID_current);
+                distMap_local(~mask) = nan;
+                edmat = [edmat; iE, ID_current, gb, arrayfun(@(x) nanmean(eMap_local(distMap_local==x)), 1:250)];
+                
+            end
+            % end of ~isempty(iS_neighbor)
+            
+        end
+
+        % disp(['iE=',num2str(iE),', iS=',num2str(iS),', ID=',num2str(struCell{iE}(iS).gID)]);
+        iS = iS + 1;
+    end
+    close(hW);
+    warning on;
+    
+    switch iE
+        case 2
+            edmatII_2 = edmat;
+            save('temp_results\saved_data.mat','edmatII_2','-append','-v7.3');
+        case 3
+            edmatII_3 = edmat;
+            save('temp_results\saved_data.mat','edmatII_3','-append','-v7.3');
+        case 4
+            edmatII_4 = edmat;
+            save('temp_results\saved_data.mat','edmatII_4','-append','-v7.3');
+        case 5
+            edmatII_5 = edmat;
+            save('temp_results\saved_data.mat','edmatII_5','-append','-v7.3');
+    end
+    
+end
+
+
+%%  The following: plot curve.
+iE = 3;
 load(['D:\p\m\DIC_Analysis\temp_results\quants_wrt_gb_',num2str(iE),'.mat']);
-load('D:\p\m\DIC_Analysis\temp_results\saved_data.mat', ['edmat_',num2str(iE)]);
+load('D:\p\m\DIC_Analysis\temp_results\saved_data.mat', ['edmatII_',num2str(iE)]);
 
-expr = ['edmat_',num2str(iE),';'];
+expr = ['edmatII_',num2str(iE),';'];
 edmat = evalin('base',expr);
 
-% nanmean
+%% nanmean
 clear eline_not_involved eline_slip_twin_a eline_slip_twin_b eline_co_found eline_twin_twin_a eline_twin_twin_b eline_slip_growth_a eline_slip_growth_b eline_co_growth
 legend_str = [];
 
@@ -315,7 +375,7 @@ ylabel('Mean of Effective strain');
 
 title(['iE=',num2str(iE)],'fontweight','normal');
 
-% median
+%%  median
 clear eline_not_involved eline_slip_twin_a eline_slip_twin_b eline_co_found eline_twin_twin_a eline_twin_twin_b eline_slip_growth_a eline_slip_growth_b eline_co_growth
 
 [ind,~] = ismember([edmat(:,3),edmat(:,2)], bg_not_involved, 'rows');
